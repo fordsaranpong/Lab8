688e7241a6c254c747802fbe8f76b7bdffc8d5fc
fordsaranpong <forddbkzero@gmail.com>
2021-01-05 18:57:50 +0700

    8.2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
+void 	printO(int x,int y){
+	int a=x,b=y;
+	if(a>0&&b>0){
+		int i,j;
 
-//Write the function printO() here
+		for( i=0;i<x;i++){
+		
+			for( j=0;j<y;j++){		
+			cout<<"O";
+			}cout<<"\n";}}
+			
+			
+	else{cout<<"Invalid input";
+	}		
+	}
 
 int main(){
 	
