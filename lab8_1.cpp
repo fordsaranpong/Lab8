21a5db2085c8fd6a74c64fe94fff1f3783ed7811
fordsaranpong <forddbkzero@gmail.com>
2021-01-05 18:25:05 +0700

    8.1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -2,14 +2,18 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){		
+	double PrevBalance,Interest,RInterest,Total,Payment,NewBalance;
 	cout << "Enter initial loan: ";
+	cin>>PrevBalance;
 	cout << "Enter interest rate per year (%): ";
+	cin>>Interest;
+	RInterest=PrevBalance*(Interest/100);
+	Total=RInterest+PrevBalance;
 	cout << "Enter amount you can pay per year: ";
+	cin>>Payment;
+	NewBalance=Total-Payment;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +21,40 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+	int i=1;
+	if(Total<=Payment){Payment=Total;NewBalance=0; 	cout << setw(13) << left <<i; 
+	cout << setw(13) << left <<PrevBalance; 
+	cout << setw(13) << left <<RInterest; 
+	cout << setw(13) << left <<Total; 
+	cout << setw(13) << left <<Payment;
+	cout << setw(13) << left <<NewBalance;
+	cout << "\n";}
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	else{
+	do{
+
+	cout << setw(13) << left <<i; 
+	cout << setw(13) << left <<PrevBalance; 
+	cout << setw(13) << left <<RInterest; 
+	cout << setw(13) << left <<Total; 
+	cout << setw(13) << left <<Payment;
+	cout << setw(13) << left <<NewBalance;
+	cout << "\n";
+	i++;
+
+	PrevBalance=NewBalance;
+	RInterest=PrevBalance*(Interest/100);
+	Total=RInterest+PrevBalance;
+	NewBalance=Total-Payment;
+	if(Total<=Payment){Payment=Total;NewBalance=0; 	cout << setw(13) << left <<i; 
+	cout << setw(13) << left <<PrevBalance; 
+	cout << setw(13) << left <<RInterest; 
+	cout << setw(13) << left <<Total; 
+	cout << setw(13) << left <<Payment;
+	cout << setw(13) << left <<NewBalance;
+	cout << "\n"; break;}
+	}while(NewBalance>=0);}
+
 	
 	return 0;
 }
